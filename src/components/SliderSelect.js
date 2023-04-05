362e66e7066bc2d165696d52f652abf488fc4bbc
DuanLeeWatkins <leeduan19@gmail.com>
2023-04-05 12:13:45 -0400

    updated slider component

--- src/components/SliderSelect.js
+++ src/components/SliderSelect.js
@@ -4,9 +4,36 @@ import SliderComponent from "./common/SliderComponent";
 const SliderSelect = () => {
   return (
     <>
-      <SliderComponent min={0} max={50} defaultValue={20} />
-      <SliderComponent min={100} max={50} defaultValue={20} />
-      <SliderComponent min={300} max={500} defaultValue={20} />
+      <SliderComponent
+        label="Home Value"
+        min={10000}
+        max={1000000}
+        defaultValue={10000}
+        step={1000}
+        onChange={(e, value) => console.log(value)}
+        unit="$"
+        amount={10000}
+      />
+      <SliderComponent
+        label="Down Payment"
+        min={0}
+        max={50}
+        defaultValue={20}
+        step={5}
+        onChange={(e, value) => console.log(value)}
+        unit="$"
+        amount={500}
+      />
+      <SliderComponent
+        label="Loan Amount"
+        min={200}
+        max={1500}
+        defaultValue={20}
+        step={5}
+        onChange={(e, value) => console.log(value)}
+        unit="$"
+        amount={700}
+      />
     </>
   );
 };
--- src/components/common/SliderComponent.js
+++ src/components/common/SliderComponent.js
@@ -1,15 +1,41 @@
 import React from "react";
 import Slider from "@mui/material/Slider";
+import { Stack, Typography } from "@mui/material";
 
-const SliderComponent = (props) => {
+const SliderComponent = ({ defaultValue, min, max, step, onChange, value, label, unit, amount }) => {
   return (
-    <Slider
-      defaultValue={props.defaultValue}
-      min={props.min}
-      max={props.max}
-      aria-label="Default"
-      valueLabelDisplay="auto"
-    />
+    <Stack my={1.4}>
+      <Stack gap={1}>
+        <Typography variant="subtitle1">{label}</Typography>
+        <Typography variant="h5">
+          {unit}
+          {amount}
+        </Typography>
+      </Stack>
+      <Slider
+        defaultValue={defaultValue}
+        min={min}
+        max={max}
+        aria-label="Default"
+        valueLabelDisplay="auto"
+        marks
+        step={step}
+        onChange={onChange}
+        value={value}
+        label={label}
+      />
+      <Stack direction="row" justifyContent="space-between">
+        <Typography variant="caption" color="text.secondary">
+          {unit}
+          {min}
+        </Typography>
+        <Typography variant="caption" color="text.secondary">
+          {unit}
+          {max}
+        </Typography>
+      </Stack>
+      <Typography>dfadssdf</Typography>
+    </Stack>
   );
 };
 
